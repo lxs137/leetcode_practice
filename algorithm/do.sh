1d14c9c6114ac97a6cbfa7eb390703db473cb17a
lxs <lxs137@hotmail.com>
2020-02-04 16:22:12 +0800

    format

--- algorithm/do.sh
+++ algorithm/do.sh
@@ -2,11 +2,11 @@
 
 # Check command param
 if [[ $# -ne 2 && $# -ne 3 ]]; then
-	echo "$0 [command] [index] <target>"
-	echo "command: init [index] [target]"
-	echo "         build [index]"
-	echo "         force-build [index]"
-	exit
+  echo "$0 [command] [index] <target>"
+  echo "command: init [index] [target]"
+  echo "         build [index]"
+  echo "         force-build [index]"
+  exit
 fi
 
 COMMAND=$1
@@ -15,37 +15,37 @@ TARGET=$3
 BINARY="test"
 
 create_source(){
-	SOURCE_FILE=$1
-	HEADER_FILE="${SOURCE_FILE//.cpp/.h}"
-	if [[ -e "$HEADER_FILE" ]]; then
-		TEXT=$(<"$HEADER_FILE")$'\n'$(<"$SOURCE_FILE")
-	else
-		TEXT=$(<"$SOURCE_FILE")
-	fi
-	TEMPLATE=$(<main.cpp.tpl)
-	echo "${TEMPLATE//class-solution-impl/$TEXT}" > "$2"
+  SOURCE_FILE=$1
+  HEADER_FILE="${SOURCE_FILE//.cpp/.h}"
+  if [[ -e "$HEADER_FILE" ]]; then
+    TEXT=$(<"$HEADER_FILE")$'\n'$(<"$SOURCE_FILE")
+  else
+    TEXT=$(<"$SOURCE_FILE")
+  fi
+  TEMPLATE=$(<main.cpp.tpl)
+  echo "${TEMPLATE//class-solution-impl/$TEXT}" > "$2"
 }
 
 if [[ "$COMMAND" = "init" ]]; then
-	if [[ ! -d "$INDEX" ]]; then
-		mkdir -p "$INDEX"
-		touch "$INDEX/$TARGET.cpp"
-	else
-		echo "dir is exist"
-	fi
+  if [[ ! -d "$INDEX" ]]; then
+    mkdir -p "$INDEX"
+    touch "$INDEX/$TARGET.cpp"
+  else
+    echo "dir is exist"
+  fi
 elif [[ "$COMMAND" = "build" ]]; then
-	SRC_PATH=$(find $INDEX -name "*.cpp")
-	create_source $SRC_PATH "main.cpp"
+  SRC_PATH=$(find $INDEX -name "*.cpp")
+  create_source $SRC_PATH "main.cpp"
   cpplint --linelength=120 --filter=-legal/copyright $SRC_PATH
-	if g++ --std=c++11 "main.cpp" -o $BINARY; then
-			echo "*****************RUN*****************"
-			./test
-	fi
+  if g++ --std=c++11 "main.cpp" -o $BINARY; then
+    echo "*****************RUN*****************"
+    ./test
+  fi
 elif [[ "$COMMAND" = "force-build" ]]; then
-SRC_PATH=$(find $INDEX -name "*.cpp")
-	if g++ --std=c++11 "main.cpp" -o $BINARY; then
-			echo "*****************RUN*****************"
-			./test
-	fi
+  SRC_PATH=$(find $INDEX -name "*.cpp")
+  if g++ --std=c++11 "main.cpp" -o $BINARY; then
+    echo "*****************RUN*****************"
+    ./test
+  fi
 fi
 
